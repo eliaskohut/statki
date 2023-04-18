f7100446cf8d472d399a5e82facbac9e8ae15dd2
unknown <73884870+misty0mist@users.noreply.github.com>
2023-04-18 22:32:46 +0200

    init commit

--- app/app.go
+++ app/app.go
@@ -1,4 +1,36 @@
 package app
 
-func start() {
+import (
+	"fmt"
+	"log"
+	"main/client"
+
+	"github.com/fatih/color"
+	board "github.com/grupawp/warships-lightgui"
+)
+
+func Start() {
+	token, game, layout, status, err := client.InitGame()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	board := board.New(
+		board.ConfigParams().
+			HitChar('#').
+			HitColor(color.FgRed).
+			BorderColor(color.BgRed).
+			RulerTextColor(color.BgYellow).
+			NewConfig())
+
+	for _, i := range layout {
+		board.Set(0, i, 4)
+	}
+
+	board.Display()
+
+	fmt.Println(token)
+	fmt.Println(game)
+	fmt.Println(layout)
+	fmt.Println(status)
 }
--- main.go
+++ main.go
@@ -1,36 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"main/client"
-
-	"github.com/fatih/color"
-	board "github.com/grupawp/warships-lightgui"
-)
+import "main/app"
 
 func main() {
-	token, game, layout, status, err := client.InitGame()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	board := board.New(
-		board.ConfigParams().
-			HitChar('#').
-			HitColor(color.FgRed).
-			BorderColor(color.BgRed).
-			RulerTextColor(color.BgYellow).
-			NewConfig())
-
-	for _, i := range layout {
-		board.Set(0, i, 4)
-	}
-
-	board.Display()
-
-	fmt.Println(token)
-	fmt.Println(game)
-	fmt.Println(layout)
-	fmt.Println(status)
+	app.Start()
 }
